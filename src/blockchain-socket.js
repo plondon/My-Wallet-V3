86956d3631b0a3269fdef0c584b4b8104c90042d
Justin Tormey <jrtormey@gmail.com>
2016-01-15 18:08:53 -0500

    fix(websocket): wait until connected to send, connect on close

--- src/blockchain-socket.js
+++ src/blockchain-socket.js
@@ -5,7 +5,6 @@ function BlockchainSocket() {
   this.wsUrl = 'wss://ws.blockchain.info/inv';
   this.headers = { 'Origin': 'https://blockchain.info' };
   this.socket;
-  this.reconnectInterval;
   this.pingInterval;
   this.reconnect;
 }
@@ -29,28 +28,43 @@ if (!(typeof window === 'undefined')) {
 
 
 BlockchainSocket.prototype.connect = function (onOpen, onMessage, onClose) {
+  this.reconnect = function (continueFunc) {
+    continueFunc = continueFunc || function () {};
+    var open    = function () { onOpen(); continueFunc(); }
+      , connect = this.connectOnce.bind(this, open, onMessage, onClose);
 
-  this.reconnect = function () {
-    var connect = this.connectOnce.bind(this, onOpen, onMessage, onClose);
-    if (!this.socket || this.socket.readyState === 3) connect();
+    // 2 = CLOSING, 3 = CLOSED (try to connect)
+    if (!this.socket || this.socket.readyState >= 2) {
+      connect();
+    }
+    // 0 = CONNECTING (try again in 10 ms)
+    else if (this.socket.readyState === 0) {
+      setTimeout(this.reconnect.bind(this, continueFunc), 10);
+    }
+    // 1 = OPEN (continue)
+    else {
+      continueFunc();
+    }
   }.bind(this);
-  var pingSocket = function () { this.send('{"op":"ping_block"}'); }.bind(this);
+
+  if (!this.pingInterval) {
+    var pingSocket = this.send.bind(this, '{"op":"ping_block"}');
+    this.pingInterval = setInterval(pingSocket, 30000);
+  }
+
   this.reconnect();
-  this.reconnectInterval = setInterval(this.reconnect, 20000);
-  this.pingInterval = setInterval(pingSocket, 30013);
 };
 
 BlockchainSocket.prototype.connectOnce = function (onOpen, onMessage, onClose) {
   this.socket = new WebSocket(this.wsUrl, [], { headers: this.headers });
   this.socket.on('open', onOpen);
   this.socket.on('message', onMessage);
-  this.socket.on('close', onClose);
+  this.socket.on('close', function () { onClose(); this.reconnect(); }.bind(this));
 };
 
 BlockchainSocket.prototype.send = function (message) {
-  this.reconnect();
-  var send = function() {this.socket.send(message); }.bind(this);
-  if (this.socket.readyState === 1) { send();}
+  var send = function () { this.socket.send(message); }.bind(this);
+  this.reconnect(send);
 };
 
 module.exports = BlockchainSocket;
